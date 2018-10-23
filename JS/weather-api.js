46f4461c2a1388f357be54396502538dbd69dae8
slopeiz <43746219+slopeiz@users.noreply.github.com>
2018-10-23 13:13:35 +0200

    testcommit
    bare for å vise en skrulling hva girhub er

---
+++ JS/weather-api.js
@@ -0,0 +1,15 @@
+// function start() {
+//   loadJSON("http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=2f79bff81a2e2dab56b41ffb789bbb0d", gotData);
+// }
+//
+//
+// function gotData(data) {
+//   weather = data;
+// }
+//
+//
+// function fremvisning() {
+//   if (weather) {
+//     utskrift = weather.main.temp, weather.main.temp;
+//   }
+// }
