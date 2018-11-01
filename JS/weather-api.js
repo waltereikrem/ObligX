205efbcdfe59ef2ba6cd3b5f6758994fef53ec8b
waltereikrem <43746219+waltereikrem@users.noreply.github.com>
2018-11-01 22:59:47 +0100

    Api oppe å går ++
    Api fungerer.
    søkefelt fungerer.
    custom illustrasjoner laget ettersom api sine egne var små og kjedelige.
    
    todo: send illustrasjoner til jan, fiks pre søk bilde taggen, stilsett api area, og fyll inn footer.

--- JS/weather-api.js
+++ JS/weather-api.js
@@ -1,4 +1,38 @@
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=2f79bff81a2e2dab56b41ffb789bbb0d", function(data) {
+window.onload = oppstart;
 
-  cosnole.log(data)
-});
+
+function oppstart() {
+
+  document.getElementById("soke-knapp").onclick = sjekk;
+
+
+  function sjekk() {
+    document.getElementById("temp").innerHTML = "";
+    var input = document.getElementById("by-input").value;
+
+
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=metric&APPID=2f79bff81a2e2dab56b41ffb789bbb0d",
+      function(data) {
+        console.log(data) //så jeg kan dobbelt sjekke grenene og vite hva jeg skal skrive ut #500iq
+
+        var by = data.name;
+        var icon = //icon linken              iconfilens navn       filtypen
+          "js/apibilder/" + data.weather[0].icon + ".png";
+        var temp = data.main.temp;
+        var weather = data.weather[0].main;
+        var luft = data.main.humidity;
+        var vind = data.wind.speed;
+
+        document.getElementById("sted").innerHTML = by;
+        $('.icon').attr('src', icon); // legger til "ikonet" som ble satt sammen i icon variablen
+        document.getElementById("temp").innerHTML ="<b> Tempratur: </b>" + temp + "&deg;C"; //kunne brukt Math.floor men foretrekker at den vise de 2 decimalene som hentes ut uansett
+        document.getElementById("weather-typen").innerHTML = "<b> Værtype: </b>" + weather;
+        document.getElementById("luftfukt").innerHTML = "<b> Luftfuktighet: </b>" + luft + "%";
+        document.getElementById("vindhast").innerHTML = "<b> Vind hastighet: </b>" + vind + "m/s";
+        
+
+
+      }
+    );
+  }
+}
