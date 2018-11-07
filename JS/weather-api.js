dbae88f85b6ec5ee799a642ebd3fff6a72be9e00
slopeiz <43746219+slopeiz@users.noreply.github.com>
2018-11-07 19:20:15 +0100

    redigering
    bilde error fikset. viste seg at stor bokstav på mappene var feilen.
    
    alle scores er å godkjente
    
    redigert api feltet til å ta med description av været og redigert api link tilå levere beskrivelser på norsk.

--- JS/weather-api.js
+++ JS/weather-api.js
@@ -10,11 +10,11 @@ function oppstart() {
   function sjekk() {
     document.getElementById("temp").innerHTML = "";
 
-    
+
     var input = document.getElementById("by-input").value;
 
 
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=metric&APPID=2f79bff81a2e2dab56b41ffb789bbb0d",
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + input + "&lang=NO&units=metric&APPID=2f79bff81a2e2dab56b41ffb789bbb0d",
       function(data) {
         console.log(data) //så jeg kan dobbelt sjekke grenene og vite hva jeg skal skrive ut #500iq
 
@@ -26,12 +26,13 @@ function oppstart() {
         var weather = data.weather[0].main;
         var luft = data.main.humidity;
         var vind = data.wind.speed;
+        var beskr= data.weather[0].description
 
         document.getElementById("sted").innerHTML = by;
         $('.icon').attr('src', icon); // legger til "ikonet" som ble satt sammen i icon variablen
         $('.icon').attr('alt', "Illustrasjon av værsøket"); // legger til "alt tag" på vildet som blir vist frem
         document.getElementById("temp").innerHTML ="<b> Tempratur: </b>" + " " + temp + "&deg;C"; //kunne brukt Math.floor men foretrekker at den vise de 2 decimalene som hentes ut uansett
-        document.getElementById("weather-typen").innerHTML = "<b> Værtype: </b>" + " " + weather;
+        document.getElementById("weather-typen").innerHTML = "<b> Værtype: </b>" + " " + weather + " / " + beskr;
         document.getElementById("luftfukt").innerHTML = "<b> Luftfuktighet: </b>" + luft + "%";
         document.getElementById("vindhast").innerHTML = "<b> Vind hastighet: </b>" + vind + "m/s";
       }
